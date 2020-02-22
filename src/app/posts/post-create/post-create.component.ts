64ef88847261e2ba0947c819132b287fbdbe1ac1
sagar-webdeveloper <sagarveershetty4@gmail.com>
2020-02-22 18:55:46 +0000

    material design added and few more features

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import {Post} from '../post.model'
 
 @Component({
   selector: 'app-post-create',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./post-create.component.css']
 })
 export class PostCreateComponent implements OnInit {
+  enteredTitle='';
+  enteredContent='';
+  @Output() postCreated= new EventEmitter<Post>()
 
   constructor() { }
 
   ngOnInit() {
+  }
+  onAddPost(){
+    const post:Post ={ title:this.enteredTitle, content:this.enteredContent};
+    this.postCreated.emit(post);
+
   }
 
 }
---
+++ src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit, Input } from '@angular/core';
+import {Post} from '../post.model'
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css']
+})
+export class PostListComponent implements OnInit {
+
+  // posts=[
+  //   {tittle:'fisrt post',content:'first post content'},
+  //   {tittle:'second post',content:'first post content'},
+  //   {tittle:'third post',content:'first post content'},
+  // ]
+ @Input() posts:Post[]=[];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
